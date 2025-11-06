ce7a95940f0ef0dbd7774ac9b67b92f825598c9e
Aman Tiwari <amant8183@gmail.com>
2025-11-06 19:20:56 +0530

    add custom validators

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -14,30 +14,30 @@ const sendEmail = async (options) => {
   const emailHtml = mailGenerator.generate(options.mailgenContent);
 
   const transporter = nodemailer.createTransport({
-  host: process.env.MAILTRAP_SMTP_HOST,
-  port: process.env.MAILTRAP_SMTP_PORT,
-  auth: {
-    user: process.env.MAILTRAP_SMTP_USER,
-    pass: process.env.MAILTRAP_SMTP_PASS,
-  },
-});
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_SMTP_USER,
+      pass: process.env.MAILTRAP_SMTP_PASS,
+    },
+  });
 
-const mail = {
-  from: "mail.taskmanager@example.com",
-  to: options.email,
-  subject: options.subject,
-  text: emailTextual,
-  html: emailHtml,
-};
+  const mail = {
+    from: "mail.taskmanager@example.com",
+    to: options.email,
+    subject: options.subject,
+    text: emailTextual,
+    html: emailHtml,
+  };
 
-try {
-  await transporter.sendMail(mail);
-} catch (error) {
-  console.error(
-    "Email service failed silently. Make sure that you have provided your MAILTRAP credentials in the .env file",
-  );
-  console.error("Error:", error);
-}
+  try {
+    await transporter.sendMail(mail);
+  } catch (error) {
+    console.error(
+      "Email service failed silently. Make sure that you have provided your MAILTRAP credentials in the .env file",
+    );
+    console.error("Error:", error);
+  }
 };
 
 const emailVerificationMailgenContent = (username, verificationUrl) => {
@@ -79,8 +79,6 @@ const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
     },
   };
 };
-
-
 
 export {
   emailVerificationMailgenContent,
