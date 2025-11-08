4a59e64760163fd420c21985ce127fadac43075a
Aman Tiwari <amant8183@gmail.com>
2025-11-09 01:31:52 +0530

    add all routes and validators for auth

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -153,7 +153,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
-    .status()
+    .status(200)
     .json(new ApiResponse(200, req.user, "Current user fetched successfully"));
 });
 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,21 +1,52 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  forgotPasswordRequest,
+  getCurrentUser,
   login,
   logoutUser,
+  refreshAccessToken,
   registerUser,
+  resendEmailVerification,
+  resetForgotPassword,
+  verifyEmail,
 } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   userRegisterValidator,
   userLoginValidator,
+  userResetForgotPasswordValidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
 } from "../validators/index.js";
 const router = Router();
 
+// unsecure routes
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/refresh-token").get(refreshAccessToken);
+router
+  .route("/forgot-password")
+  .get(userForgotPasswordValidator(), validate, forgotPasswordRequest);
+router
+  .route("/reset-password/:resetToken")
+  .get(userResetForgotPasswordValidator(), validate, resetForgotPassword);
 
 // secure routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router
+  .route("/change-password")
+  .post(
+    verifyJWT,
+    userChangeCurrentPasswordValidator(),
+    validate,
+    changeCurrentPassword,
+  );
+router
+  .route("/resend-email-verification")
+  .post(verifyJWT, resendEmailVerification);
 
 export default router;
